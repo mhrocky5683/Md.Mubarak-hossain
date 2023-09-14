63ba1a1e35847d41eca604714a2ccc7e2b17bcfa
Md.Mubarak Hossain <136848721+mhrocky5683@users.noreply.github.com>
2023-09-15 01:44:21 +0600

    Add files via upload

---
+++ Problem -05.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Input: A number to be checked
+  int number = 9;
+
+  // Check if the number is even or odd
+  if (number % 2 == 0) {
+    print('$number is Even.');
+  } else {
+    print('$number is Odd.');
+  }
+}
